62af09b7f207cc5d293221efb6c5da3b4e7e7d85
Void Radiance <34035673+voidradiance@users.noreply.github.com>
2022-09-21 19:58:42 +0700

    Add files via upload

---
+++ AgungSetiawan_Exercise1_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgungSetiawan_Exercise1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Masukkan tanggal ulang anda :");
+            int tanggal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukan bulan ulang tahun anda :");
+            int bulan = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukan tahun ulang tahun anda :");
+            int tahun = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(tahun,bulan,tanggal);
+
+            TimeSpan umur = DateTime.Now - birthDate;
+            int umurNow = Convert.ToInt32(umur.TotalDays) / 365;
+
+            Console.WriteLine("Masukan nama anda :");
+            string namaAnda = Console.ReadLine();
+
+            Console.WriteLine("Menghitung umur {0}",namaAnda);
+            Console.WriteLine("Umur anda adalah {0} tahun", umurNow);
+
+        }
+    }
+}
